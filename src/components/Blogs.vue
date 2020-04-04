da59f58e04a6d0e982d3302e32e9ec2086614291
DanyVi <vinci.dani@gmail.com>
2020-04-04 18:33:13 -0400

    axios request to jsonplaceholder and filter

--- src/components/Blogs.vue
+++ src/components/Blogs.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="blogs">
     <h2>Blogs</h2>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body | snippet }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Blogs',
   data(){
     return {
-
+      posts: []
     }
   },
   methods: {
   },
+  // good hook to grab external data
   created(){
+    axios
+    .get('https://jsonplaceholder.typicode.com/posts/')
+    .then(response => {
+      // console.log(response.data);
+      this.posts = response.data;
+    }) 
+    .catch(err => {
+      // catch takes in any error obj as a parameter into this es6 function
+      console.log(err);
+      
+    })
   },
 
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// create a global filter 
+// .filter() takes in 2 parameter:
+// 1) name of the filter
+// 2) function that roms through the values that we are trying to output in the browser to manipulate and return an updated value and output to the browser
+// it will be an arrow function that takes in a val (value)
+Vue.filter('snippet', val=>{
+  if (!val||typeof(val)!='string') {
+     return ''
+  }
+  val = val.slice(0,50)
+  return val
+})
+
 Vue.config.productionTip = false
 
 new Vue({
